dd820f679f804ee8d1bc2b5479bbfb657286d79f
Lada Krofingerová <xkrofi00@stud.fit.vutbr.cz>
2022-12-10 13:45:04 +0100

    Fifth day

---
+++ 5/5.py
@@ -0,0 +1,33 @@
+def parseText(stack_txt):
+    stack = [[]]*9
+    for s in stack_txt:
+        a = 0
+        if s.startswith(" 1"):
+            break
+        for i in range(9):
+            x = s[a:a+3]
+            a += 4
+            stack[i].append(x)
+    return stack
+
+
+lines = []
+with open('input.txt') as f:
+    lines = f.readlines()
+stack = []
+i = 0
+for l in lines:
+    if l == "\n":
+        i += 1
+        break
+    stack.append(l)
+    i += 1
+instructions = lines[i:]
+# print("stack:")
+# print(stack)
+# print("instructions:")
+# print(instructions)
+
+s = parseText(stack)
+print(s[0])
+
