8e85912eb9a604a8a3a836e4940c0bf433024a66
Lada Krofingerova <lada.krofingerova@sap.com>
2022-12-14 11:34:36 +0100

    DAY 7
    Hopefully parsing the input right... Time to find the right number now
    BUT in work sooo

--- 7/7.py
+++ 7/7.py
@@ -1,21 +1,35 @@
 class directory:
     name = ""
     stuff = []
+    size = 0
     def __init__(self, name):
         self.name = name
 
     def addDir(self, dirName):
         newDir = directory(dirName)
-        stuff.append(newDir)
+        self.stuff.append(newDir)
 
     def addFile(self, fileName, size):
-        newFile = {"name": fileName, "size", size}
-        stuff.append(newFile)
+        newFile = {"name": fileName, "size": size}
+        self.stuff.append(newFile)
+        self.size += int(size)
 
     def getName(self):
         return self.name
 
     def getSub(self, name):
+        for x in self.stuff:
+            if type(x) == directory:
+                if x.getName() == name:
+                    return x
+    def getSize(self):
+        # size = 0
+        # for x in self.stuff:
+          #  if type(x) == directory:
+           #     size += x.getSize()
+            #else:
+             #   size += int(x["size"])
+        return self.size
 
 
 lines = []
@@ -27,15 +41,16 @@ currentDirectory = main
 previousDirectory = []
 copy = False
 for x in lines:
-    if x == "$ cd /":
+    if x == "$ cd /\n":
         previousDirectory.clear()
         currentDirectory = main
-    elif x == "$ cd ..":
+    elif x == "$ cd ..\n":
         currentDirectory = previousDirectory.pop()
     elif x.startswith("$ cd"):
         previousDirectory.append(currentDirectory)
-
-    elif x == "$ ls":
+        y = x.split(" ")
+        newDir = currentDirectory.getSub(y[2])
+    elif x == "$ ls\n":
         continue
     elif x.startswith("dir"):
         z = x.split(" ")
@@ -43,3 +58,6 @@ for x in lines:
     else:
         z = x.split(" ")
         currentDirectory.addFile(z[1], z[0])
+
+print(main)
+print("Velikost domovske slozky: ", main.getSize())
